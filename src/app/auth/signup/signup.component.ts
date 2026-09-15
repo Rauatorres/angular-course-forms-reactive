aba36e8551285bb5ff4a81e98731a60201993d16
rauatorres <rauatorres@gmail.com>
2026-09-15 16:45:34 -0300

    fazendo formulário de cadastrar

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
+  imports: [ReactiveFormsModule],
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.form.value.email);
+    console.log(this.form.value.password);
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+}
