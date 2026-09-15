cc2073242bc44fd50de61a5bbe0cd2e2f925ef81
rauatorres <rauatorres@gmail.com>
2026-09-15 11:20:25 -0300

    adicionando formulário e submit modo reactive

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.form.value.email, this.form.value.password);
+  }
+}
