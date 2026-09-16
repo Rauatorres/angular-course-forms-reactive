d40e2a5848d028dda5338b3b6b6a315e3403257b
rauatorres <rauatorres@gmail.com>
2026-09-16 16:00:11 -0300

    aninhando os formulários

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -22,36 +22,29 @@ import { of } from 'rxjs';
 export class SignupComponent implements OnInit {
   // private destroyRef = inject(DestroyRef);
 
-  form = new FormGroup(
-    {
-      email: new FormControl('', {
-        validators: [Validators.email, Validators.required],
-      }),
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    passwordGroup: new FormGroup({
       password: new FormControl('', {
         validators: [Validators.required, Validators.minLength(6)],
       }),
       confirmPassword: new FormControl(''),
-      firstName: new FormControl('', { validators: [Validators.required] }),
-      lastName: new FormControl('', { validators: [Validators.required] }),
+    }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
       street: new FormControl('', { validators: [Validators.required] }),
       number: new FormControl('', { validators: [Validators.required] }),
       postalCode: new FormControl('', { validators: [Validators.required] }),
       city: new FormControl('', { validators: [Validators.required] }),
-      role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
-        validators: [Validators.required],
-      }),
-      agree: new FormControl(false, [Validators.required]),
-    },
-    {
-      validators: [
-        (control) => {
-          return control.get('password')?.value == control.get('confirmPassword')?.value
-            ? null
-            : { confirmPasswordIsInvalid: true };
-        },
-      ],
-    },
-  );
+    }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
+      validators: [Validators.required],
+    }),
+    agree: new FormControl(false, [Validators.required]),
+  });
 
   get emailIsInvalid() {
     return (
@@ -63,24 +56,24 @@ export class SignupComponent implements OnInit {
 
   get passwordIsInvalid() {
     return (
-      this.form.controls.password.invalid &&
-      this.form.controls.password.touched &&
-      this.form.controls.password.dirty
+      this.form.controls.passwordGroup.controls.password.invalid &&
+      this.form.controls.passwordGroup.controls.password.touched &&
+      this.form.controls.passwordGroup.controls.password.dirty
     );
   }
 
-  get confirmPasswordIsInvalid() {
-    return (
-      this.form.hasError('confirmPasswordIsInvalid') &&
-      (this.form.controls.confirmPassword.dirty || this.form.controls.password.dirty)
-    );
-  }
+  // get confirmPasswordIsInvalid() {
+  //   return (
+  //     this.form.hasError('confirmPasswordIsInvalid') &&
+  //     (this.form.controls.confirmPassword.dirty || this.form.controls.password.dirty)
+  //   );
+  // }
 
   ngOnInit(): void {}
 
   onSubmit() {
     console.log(this.form.value.email);
-    console.log(this.form.value.password);
+    console.log(this.form.controls.passwordGroup.value.password);
   }
 
   onReset() {
