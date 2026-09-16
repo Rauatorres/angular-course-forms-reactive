712e40c2dddf325fecd6b73fec346801c4385d47
rauatorres <rauatorres@gmail.com>
2026-09-16 15:37:59 -0300

    adicionando confirmar senha e outros campos do formulário

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { of } from 'rxjs';
 
 // function confirmPasswordIsValid(control: AbstractControl){
 //   if(control.value != )
@@ -18,18 +19,39 @@ import {
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {
-  form = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.email, Validators.required],
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
-    }),
-    repeatPassword: new FormControl('', {
-      validators: [Validators.required],
-    }),
-  });
+export class SignupComponent implements OnInit {
+  // private destroyRef = inject(DestroyRef);
+
+  form = new FormGroup(
+    {
+      email: new FormControl('', {
+        validators: [Validators.email, Validators.required],
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl(''),
+      firstName: new FormControl('', { validators: [Validators.required] }),
+      lastName: new FormControl('', { validators: [Validators.required] }),
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
+      role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
+        validators: [Validators.required],
+      }),
+      agree: new FormControl(false, [Validators.required]),
+    },
+    {
+      validators: [
+        (control) => {
+          return control.get('password')?.value == control.get('confirmPassword')?.value
+            ? null
+            : { confirmPasswordIsInvalid: true };
+        },
+      ],
+    },
+  );
 
   get emailIsInvalid() {
     return (
@@ -46,6 +68,15 @@ export class SignupComponent {
       this.form.controls.password.dirty
     );
   }
+
+  get confirmPasswordIsInvalid() {
+    return (
+      this.form.hasError('confirmPasswordIsInvalid') &&
+      (this.form.controls.confirmPassword.dirty || this.form.controls.password.dirty)
+    );
+  }
+
+  ngOnInit(): void {}
 
   onSubmit() {
     console.log(this.form.value.email);
